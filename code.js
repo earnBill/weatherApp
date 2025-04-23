3072e88d91564412115d39ac2d5fa01c6202b9e9
BillLaptop <earnappbill@gmail.com>
2025-04-23 19:00:15 +0300

    fix css

--- code.js
+++ code.js
@@ -2,10 +2,12 @@ const inputCity = document.querySelector('.city-name');
 const searchButton = document.querySelector('.search-button');
 const city = document.querySelector('.city');
 const weatherInfo = document.querySelector('.weather-info');
+const time = document.querySelector('.time');
+const description = document.querySelector('.description');
 const weatherImage = document.querySelector('.weather-image');
+const temprature = document.querySelector(".temprature");
 
-
-let cityName;
+let cityName = 'Athens';
 let weatherPic;
 
 // async function getWeather() {
@@ -15,12 +17,10 @@ let weatherPic;
 //     console.log(weather.address);
 // }
 
-// getWeather()
+getWeather();
+console.log('eeeeeeeeeeeeeeee');
 
 function getWeather() {
-  cityName = inputCity.value;
-  console.log(cityName);
-
   fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=LE3MMXGG34B5UKT9653UQ7KAL`)
   .then((response) => {
     return response.json()
@@ -28,9 +28,12 @@ function getWeather() {
       console.log(weather);
       console.log(weather.address);
       console.log(weather.currentConditions.conditions);
-      weatherPic = weather.currentConditions.conditions;
+      weatherPic = weather.currentConditions.icon;
       city.textContent = weather.resolvedAddress;
+      time.textContent = weather.currentConditions.datetime.slice(0,5);
+      temprature.textContent = weather.currentConditions.temp + '°F';
       weatherInfo.textContent = weather.currentConditions.conditions;
+      description.textContent = weather.description;
       getPhoto();
   }).catch( error => {
       console.log(error);
@@ -51,10 +54,9 @@ function getPhoto() {
 }
 
 searchButton.addEventListener('click', ()=> {
-   getWeather();
+  cityName = inputCity.value;
+  console.log(cityName);
+  getWeather();
 })
-
-
-
 
 
